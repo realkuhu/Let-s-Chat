402392ad05cbbff829232458ae1291f02b60653d
realkuhu <85197949+realkuhu@users.noreply.github.com>
2022-04-13 13:18:15 -0400

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -11,3 +11,29 @@ const firebaseConfig = {
   if (!firebase.apps.length){
     firebase.initializeApp(firebaseConfig);
   }
+user_name = localStorage.getItem("user_name")
+
+  function addRoom(){
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+      purpose : "adding room name"
+    });
+
+    
+  }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+//Start code
+console.log("Room Name - " + Room_names);
+row = '<div class="room_name" id='+Room_names+'onclick="redirectToRoomName(this.id)"> #' + Room_names +'</div><hr>'
+document.getelement
+//End code
+});});}
+getData();
+
+function redirectToRoomName (name){
+  localStorage.setItem("room_name", name);
+window.location = "chat_page.html";
+}
